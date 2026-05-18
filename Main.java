2758d9fba9cdc888f696b73c702a2d6bb09fcd17
Ankita Sahu <ankitasahu83964@gmail.com>
2026-05-18 19:32:59 +0530

    Added Main.java program

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+
+    static int power(int a, int b) {
+
+        if (b == 0) {
+            return 1;
+        }
+
+        return a * power(a, b - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter base: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter power: ");
+        int b = sc.nextInt();
+
+        System.out.println("Answer = " + power(a, b));
+    }
+}
